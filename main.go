74463610c7a855c5d8daccb22560c6a7fcaa3265
alexey-ringo <alexey.ringo@gmail.com>
2026-04-16 14:08:29 +0300

    Create TG-client

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/alexey-ringo/go-native-telegram-bot/clients/telegram"
+)
+
+const (
+	tgBotHost = "api.telegram.org"
+)
+
+func main() {
+	tgClient = telegram.New(tgBotHost, mustToken())
+
+}
+
+func mustToken() string {
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is empty")
+	}
+
+	return *token
+}
